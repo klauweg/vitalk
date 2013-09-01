f8765851d0c20c56bef2936179f1db10393b60e3
Klaus W <vortex25@gmx.de>
2013-09-01 17:25:06 +0200

    development

--- vito_io.c
+++ vito_io.c
@@ -59,6 +59,7 @@ void closetty( void )
 }
 
 // Keine Rückgabewerte. Wenns nicht geht wird sowieso beendet:
+// Initialisieren des 300 Protokolls:
 void vito_init( void )
 {
    int trys;
@@ -66,6 +67,7 @@ void vito_init( void )
    const unsigned char initKw[] = { 0x04 };
    const unsigned char initSeq[] = { 0x16, 0x00, 0x00 };
    
+   // So lange 0x04 senden bis 0x05 empfangen wird:
    trys = 5;
    fprintf( stderr, "Reset Communication to KW Proto." );
    do
@@ -86,6 +88,7 @@ void vito_init( void )
    while ( rec != 0x05 );
    fprintf( stderr, "Success.\n");
    
+   // Wenn 0x05 empfangen wurde, Proto 300 Init:
    fprintf( stderr, "Try Proto 300 Init: " );
    write( fd_tty, initSeq, 3 );
    read( fd_tty, &rec, 1 );
@@ -98,6 +101,7 @@ void vito_init( void )
    fprintf( stderr, "Success.\n");
 }
 
+// Zurückschalten ins KW Protokoll:
 void vito_close( void )
 {
    const unsigned char initKw[] = { 0x04 };
@@ -119,12 +123,12 @@ static int calcCRC( unsigned char *buffer )
   return crc;
 }
 
-// Debug Ausgabe für Telegramme:
+// Debug Ausgabe Array als Hexadezimal:
 static int print_hex( unsigned char *buffer, int len )
 {
   int i;
   
-  fprintf( stderr, "Telegramm:" );
+  fprintf( stderr, "Data:" );
   for ( i = 0; i < len; i++ )
     fprintf( stderr, " 0x%02x", buffer[i] );
   fprintf( stderr, "\n" );
@@ -133,27 +137,26 @@ static int print_hex( unsigned char *buffer, int len )
   // Empfangen 06 41 07 01 01 55 25 02 07 01 8D
 }
 
-// Speicherbereich von Vitodens anfragen:
-int vito_request( int location, int size, unsigned char *vitomem )
+// Lowlevel Kommunikation mit der Vitodens:
+// Reine Nutzdaten ohne Telegrammlängenfeld und CRC werden übergeben:
+// (Wird für Schreib- und Lesezugriff benutzt)
+static int vito_meeting( unsigned char *tx_data, int tx_data_len, 
+			 unsigned char *rx_data )
 {
-  unsigned char command[20];
-  unsigned char rx_buffer[300];
+  unsigned char buffer[300];
   unsigned char rec;
-  int result;
+  int rx_data_len;
+  int i;
   
-  // Hier wird das Anfragetelegramm gebastelt:
-  command[0] = 0x41;    // Start of Frame
-  command[1] = 1+1+2+1; // Länge ab ToM: ToM(1), R/W(1), Adresse(2), Anzahl(1) (ohne CRC)
-  command[2] = 0x00;    // Type of Message: Anfrage
-  command[3] = 0x01;    // Lesezugriff
-  command[4] = (location >> 8) & 0xff; // high byte
-  command[5] = location & 0xff; // low byte
-  command[6] = size;    // Anzahl der angeforderten Bytes
-  command[7] = calcCRC( command ); // CRC berechnen
+  // Construct TX Frame:
+  buffer[0] = 0x41;     // Start of Frame
+  buffer[1] = tx_data_len;  // Länge der Nutzdaten eintragen
+  memcpy( &buffer[2], tx_data, tx_data_len ); // Nutzdaten kopieren
+  buffer[tx_data_len+2] = calcCRC( buffer ); // CRC berechnen
 
   // Debug output
   if (frame_debug)
-    print_hex( command, command[1]+3 );
+    print_hex( buffer, tx_data_len+3 );
 
   // Zur Sicherheit:
   if ( fd_tty == 0 )
@@ -162,54 +165,78 @@ int vito_request( int location, int size, unsigned char *vitomem )
       return -1;
     }
   
-  // Anfrage zur Vitodens senden:
+  //////////// Anfrage zur Vitodens senden:
   tcflush( fd_tty, TCIOFLUSH );
-  if ( write( fd_tty, command, command[1]+3 ) < command[1]+3 ) // payload + overhead
+  if ( write( fd_tty, command, tx_data_len+3 ) < tx_data_len+3 ) // payload + overhead
     {
       fprintf( stderr, "Write to tty failed.\n" );
       return -1;
     }
-
+  
+  //////////// Antwort verarbeiten:
   // Got ACK?
   if ( read( fd_tty, &rec, 1 ) < 1 )
     {
-      fprintf( stderr, "No ACK on Memory Request! (got nothing)\n" );
+      fprintf( stderr, "No ACK on Transmission! (got nothing)\n" );
       return -1;
     }
   if ( rec != 0x06 )
     {
-      fprintf( stderr, "No ACK on Memory Request! (got %x)\n", rec );
+      fprintf( stderr, "No ACK on Transmission! (got %x)\n", rec );
       return -1;
     }
-
   // Got Answer Frame Start?
-  if ( read( fd_tty, &rx_buffer[0], 1 ) < 1 )
+  if ( read( fd_tty, &buffer[0], 1 ) < 1 )
     {
       fprintf( stderr, "No Frame Start! (got nothing)\n" );
       return -1;
     }
-  if ( rx_buffer[0] != 0x41 )
+  if ( buffer[0] != 0x41 )
     {
-      fprintf( stderr, "No Frame Start! (got %x)\n", rx_buffer[0] );
+      fprintf( stderr, "No Frame Start! (got %x)\n", buffer[0] );
       return -1;
     }
-sleep(1);  
-  // Rest des Telegramms empfangen:
-  if ( read( fd_tty, &rx_buffer[1], 1 ) < 1 ) // Hier empfangen wir die länge des Telegramms
+  // Telegrammlänge empfangen:
+  if ( read( fd_tty, &buffer[1], 1 ) < 1 )
     {
       fprintf( stderr, "No Frame Size received!\n" );
       return -1;
     }
-  if ( (result = read( fd_tty, &rx_buffer[2], rx_buffer[1] + 1 )) < rx_buffer[1] + 1 ) // (+ CRC!)
-    {
-      fprintf( stderr, "Answer Frame too short! (got %d additional bytes):\n", result );
-      print_hex( rx_buffer, result + 2 ); // Rest + Start of Frame + Len of Frame
-      return -1;
-    }
-  
+  rx_data_len = buffer[1];
+  // Payload + CRC empfangen:
+  for ( i = 0; i <= rx_data_len; i++ )
+    if ( read( fd_tty, &buffer[i+2], 1 ) < 1 )
+      {
+        fprintf( stderr, "Answer Frame too short! (received %d bytes of %d expected):\n", i, rx_data_len+1 );
+	print_hex( buffer, i+2 );
+	return -1;
+      }
+
   // Debug output
   if (frame_debug)
-    print_hex( rx_buffer, rx_buffer[1] + 3 );
+    print_hex( rx_buffer, rx_data_len + 3 );
+  
+// hier fehlt noch was!
+}
+    
+
+  
+  
+  
+// Speicherbereich von Vitodens anfragen:
+int vito_request( int location, int size, unsigned char *vitomem )
+{
+  unsigned char command[20];
+  int result;
+  
+  // Hier wird das Anfragetelegramm gebastelt:
+  command[0] = 0x00;    // Type of Message: Anfrage
+  command[1] = 0x01;    // Lesezugriff
+  command[2] = (location >> 8) & 0xff; // high byte
+  command[3] = location & 0xff; // low byte
+  command[4] = size;    // Anzahl der angeforderten Bytes
+  result = vito_meeting( command, 5, command );
+
   
 }
 
