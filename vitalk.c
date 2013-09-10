f3c201e07935eda5164b1314202cae72f7cabf89
Klaus W <vortex25@gmx.de>
2013-09-10 10:59:39 +0200

    Testimplementierung für tcp server.

--- vitalk.c
+++ vitalk.c
@@ -15,6 +15,8 @@
 #include "mysql.h"
 #include "text_log.h"
 
+#define PORT 83
+
 // Signal Handler:
 void exit_handler( int exitcode )
 {
@@ -39,6 +41,19 @@ int main(int argc, char **argv)
   // Server:
   struct timeval *timeout;
   timeout = (struct timeval *) malloc( sizeof(struct timeval) );
+  
+  fd_set master; /* master file descriptor list */
+  fd_set read_fds; /* temp file descriptor list for select() */
+  struct sockaddr_in serveraddr; /* server address */
+  struct sockaddr_in clientaddr; /* client address */
+  int fdmax; /* maximum file descriptor number */
+  int listener; /* listening socket descriptor */
+  int newfd; /* newly accept()ed socket descriptor */
+  int yes = 1; /* for setsockopt() SO_REUSEADDR, below */
+  socklen_t addrlen;
+  int i;
+  char buf[1024]; /* buffer for client data */
+  int nbytes;
   
   // Option processing with GNU getopt
   while ((c = getopt (argc, argv, "H:U:P:D:T:hft:c:se")) != -1)
@@ -134,14 +149,87 @@ int main(int argc, char **argv)
       // löschen wir vorher besser den Bildschirm:
       printf("\033[2J\033[;H");
     }
+
+  /* clear the master and temp sets */
+  FD_ZERO(&master);
+  FD_ZERO(&read_fds);
+  /* create socket */
+  if((listener = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+    {
+      fprintf( stderr, "Error creating Socket!\n");
+      exit(1);
+    }
+   if(setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1)
+    {
+      fprintf( stderr, "Error configuring Socket!\n");
+      exit(1);
+    }
+  /* bind */
+  serveraddr.sin_family = AF_INET;
+  serveraddr.sin_addr.s_addr = INADDR_ANY;
+  serveraddr.sin_port = htons(PORT);
+  memset(&(serveraddr.sin_zero), '\0', 8);
+  if(bind(listener, (struct sockaddr *)&serveraddr, sizeof(serveraddr)) == -1)
+    {
+      fprintf( stderr, "Error bind Socket!\n");
+      exit(1);
+    }
+  /* listen */
+  if(listen(listener, 10) == -1)
+   {
+      fprintf( stderr, "Error listen Socket!\n");
+      exit(1);
+   }
+  /* add the listener to the master set */
+  FD_SET(listener, &master);
+  /* keep track of the biggest file descriptor */
+  fdmax = listener; /* so far, it's this one*/
+
   
   // Main Event-Loop. Kann nur durch die Signalhandler beendet werden.
   for (;;)
     {
-      if ( select ( 0, NULL, NULL, NULL, timeout ) > 0 )
+      read_fds = master;
+      if ( select ( fdmax+1, &read_fds, NULL, NULL, timeout ) > 0 )
 	{
-	  // FD i/o
-	  ;
+	  /*run through the existing connections looking for data to be read*/
+	  for(i = 0; i <= fdmax; i++)
+	    {
+	      if(FD_ISSET(i, &read_fds))
+		{ /* we got one... */
+		  if(i == listener)
+		    { /* handle new connections */
+		      addrlen = sizeof(clientaddr);
+		      if((newfd = accept(listener, (struct sockaddr *)&clientaddr, &addrlen)) == -1)
+			{
+			  fprintf( stderr, "Error accepting Connection!\n");
+			  exit(1);
+			}
+		      else
+			{
+			  FD_SET(newfd, &master); /* add to master set */
+			  if(newfd > fdmax) /* keep track of the maximum */
+			    fdmax = newfd;
+			  printf("New connection from %s on socket %d\n", inet_ntoa(clientaddr.sin_addr), newfd);
+			}
+		    }
+		  else
+		    { /* handle data from a client */
+		      if((nbytes = recv(i, buf, sizeof(buf), 0)) <= 0)
+			{ /* got error or connection closed by client */
+			  if(nbytes == 0)
+			    /* connection closed */
+			     printf("socket %d hung up\n", i);
+			  close(i); /* close it */
+			  FD_CLR(i, &master); /* remove from master set */
+			}
+		      else
+			{
+			  printf("%s", buf);
+			}
+		    }
+		}
+	    }
 	}
       else
 	{
