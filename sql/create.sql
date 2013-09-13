b577afe37317503a3dcdbb7a7fe757473e02abf0
Klaus W <vortex25@gmx.de>
2013-09-14 00:13:17 +0200

    development

--- sql/create.sql
+++ sql/create.sql
@@ -2,146 +2,62 @@
 # daher darauf.
 # MyISAM Tabellen dürften für unseren Zweck effizienter sein.
 
-create table vitolog_config ( timestamp timestamp
-                       mode TINYINT(1),
-		       warmwasser_soll TINYINT(2),
-		       runtime INT,
-		       raum_soll TINYINT(2),         # 1°
-		       raum_soll_red TINYINT(2),     # 1°
-		       neigung TINYINT,              # 0,1
-		       niveau TINYINT,               # 1°
-
-# Hier wird nur geloggt, wenn der Brenner oder pumpe auch läuft, aber dafür
-# mit hoher Auflösung:
+# Diese Werte rechnen wir der Konfiguration zu und loggen
+# nur, wenn sich etwas verändert hat:
+CREATE TABLE vitolog_config (
+      timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,  # 4 byte
+      mode TINYINT(1) UNSIGNED NOT NULL,              # 0-2
+      warmwasser_soll TINYINT(2) UNSIGNED NOT NULL,   # 1°
+      warmwasser_offset TINYINT(2) UNSIGNED NOT NULL, # 1°
+      raum_soll TINYINT(2) UNSIGNED NOT NULL,         # 1°
+      raum_soll_red TINYINT(2) UNSIGNED NOT NULL,     # 1°
+      neigung TINYINT UNSIGNED NOT NULL,              # 0,1
+      niveau TINYINT UNSIGNED NOT NULL,               # 1K
+   ) ENGINE = MyISAM, ROW_FORMAT=FIXED;
+   
+# Hier wird nur geloggt, wenn der Brenner oder Pumpe auch läuft, aber dafür
+# mit hoher Auflösung (10sec.):
 CREATE TABLE IF NOT EXISTS vitolog_kessel (
-               timestamp timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL PRIMARY KEY,  # 4byte
-	       start_count INT UNSIGNED NOT NULL,           # Zähler (4byte)
-	       ventil TINYINT(1) UNSIGNED NOT NULL,         # 1-3
-	       pumpe TINYINT(3) UNSIGNED NOT NULL,          # %
-	       flow SMALLINT UNSIGNED NOT NULL,             # l/h
-	       leistung TINYINT(3) UNSIGNED NOT NULL,       # %
-	       kessel_soll SMALLINT(3) NOT NULL,            # 0,1 °C
-	       kessel_ist SMALLINT(3) NOT NULL,             # 0,1 °C
-	       abgas SMALLINT(3) NOT NULL,                  # 0,1 °C
-	       INDEX( start_count )
-	    ) ENGINE = MyISAM, ROW_FORMAT=FIXED;
+      timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL PRIMARY KEY,  # 4byte
+      start_count INT UNSIGNED NOT NULL,           # Zähler (4byte)
+      ventil TINYINT(1) UNSIGNED NOT NULL,         # 0-3
+      pumpe TINYINT(3) UNSIGNED NOT NULL,          # %
+      flow SMALLINT UNSIGNED NOT NULL,             # l/h
+      leistung TINYINT(3) UNSIGNED NOT NULL,       # %
+      kessel_soll SMALLINT(3) NOT NULL,            # 0,1 °C
+      kessel_ist SMALLINT(3) NOT NULL,             # 0,1 °C
+      abgas SMALLINT(3) NOT NULL,                  # 0,1 °C
+      INDEX( start_count )
+   ) ENGINE = MyISAM, ROW_FORMAT=FIXED;
 	    
 
-
-# Hier gibts die Daten der ca. letzten Stunde in hoher zeitlicher Auflösung:
-create table vitolog_live ( timestamp timestamp,     # default: current_timestamp
-                       kessel_soll SMALLINT(3),
-		       kessel_ist SMALLINT(3),       # 0,1
-		       kessel_abgas SMALLINT(3),     # 0,1
-		       warmwasser_offset TINYINT(2),
-		       warmwasser_ist SMALLINT(3),   # 0,1
-		       aussentemp SMALLINT(3),       # 0,1
-		       aussentemp_ged SMALLINT(3),   # 0,1
-		       starts INT,
-		       leistung TINYINT(3),           # %
-		       ventil TINYINT(1),
-		       pumpe TINYINT(3),             # %
-		       flow SMALLINT,                # l/h
-		       vorlauf_soll TINYINT(2),      # 1°
-		       error1 TINYINT,
-		       error2 TINYINT,
-		       error3 TINYINT,
-		       error4 TINYINT,
-		       error5 TINYINT,
-		       error6 TINYINT,
-		       error7 TINYINT,
-		       error8 TINYINT,
-		       error9 TINYINT,
-		       error10 TINYINT
-		       );
-
-# Hier gibts die Daten der ca. letzten Stunde in hoher zeitlicher Auflösung:
-create table vitolog_live ( timestamp timestamp,     # default: current_timestamp
-                       mode TINYINT(1),
-                       kessel_soll SMALLINT(3),
-		       kessel_ist SMALLINT(3),       # 0,1
-		       kessel_abgas SMALLINT(3),     # 0,1
-		       warmwasser_soll TINYINT(2),
-		       warmwasser_offset TINYINT(2),
-		       warmwasser_ist SMALLINT(3),   # 0,1
-		       aussentemp SMALLINT(3),       # 0,1
-		       aussentemp_ged SMALLINT(3),   # 0,1
-		       starts INT,
-		       runtime INT,
-		       leistung TINYINT(3),           # %
-		       ventil TINYINT(1),
-		       pumpe TINYINT(3),             # %
-		       flow SMALLINT,                # l/h
-		       vorlauf_soll TINYINT(2),      # 1°
-		       raum_soll TINYINT(2),         # 1°
-		       raum_soll_red TINYINT(2),     # 1°
-		       neigung TINYINT,              # 0,1
-		       niveau TINYINT,               # 1°
-		       error1 TINYINT,
-		       error2 TINYINT,
-		       error3 TINYINT,
-		       error4 TINYINT,
-		       error5 TINYINT,
-		       error6 TINYINT,
-		       error7 TINYINT,
-		       error8 TINYINT,
-		       error9 TINYINT,
-		       error10 TINYINT
-		       );
-
-# Hier schreiben wir die Daten nur sehr selten (ca. eine Stunde?)
-create table vitolog_archiv ( timestamp timestamp,     # default: current_timestamp
-                       mode TINYINT(1),
-                       kessel_soll SMALLINT(3),
-		       kessel_ist SMALLINT(3),       # 0,1
-		       kessel_abgas SMALLINT(3),     # 0,1
-		       warmwasser_soll TINYINT(2),
-		       warmwasser_offset TINYINT(2),
-		       warmwasser_ist SMALLINT(3),   # 0,1
-		       aussentemp SMALLINT(3),       # 0,1
-		       aussentemp_ged SMALLINT(3),   # 0,1
-		       starts INT,
-		       runtime INT,
-		       leistung TINYINT(3),           # %
-		       ventil TINYINT(1),
-		       pumpe TINYINT(3),             # %
-		       flow SMALLINT,                # l/h
-		       vorlauf_soll TINYINT(2),      # 1°
-		       raum_soll TINYINT(2),         # 1°
-		       raum_soll_red TINYINT(2),     # 1°
-		       neigung TINYINT,              # 0,1
-		       niveau TINYINT,               # 1°
-		       error1 TINYINT,
-		       error2 TINYINT,
-		       error3 TINYINT,
-		       error4 TINYINT,
-		       error5 TINYINT,
-		       error6 TINYINT,
-		       error7 TINYINT,
-		       error8 TINYINT,
-		       error9 TINYINT,
-		       error10 TINYINT
-		       );
+# Hier kommen Werte die sich langsam verändern und werden daher nur seltener
+# geloggt, dafür aber unabhängig vom Betriebszustand (60sec.):
+CREATE TABLE vitolog_slow (
+      timestamp TIMESTAMP CURRENT_TIMESTAMP NOT NULL PRIMARY KEY,   # 4 byte
+      runtime INT UNSIGNED NOT NULL,                  # Zähler, Sekunden, 4byte
+      warmwasser_ist SMALLINT(3) UNSIGNED NOT NULL,   # 0,1°C
+      aussentemp SMALLINT(3) NOT NULL,                # 0,1°C
+      aussentemp_ged SMALLINT(3) NOT NULL,            # 0,1°C
+      vorlauf_soll SMALLINT(3) NOT NULL               # 0,1°C
+      kessel_ist SMALLINT(3) NOT NULL,                # 0,1°C (redundant in vitolog_kessel)
+   ) ENGINE = MyISAM, ROW_FORMAT=FIXED;
 
 
-# Hier werden die Daten zwar in hoher Zeitauflösung und auch langfristig gespeichert, aber
-# nur die interessanten Werte:
-create table vitolog_hires ( timestamp timestamp,     # default: current_timestamp
-                       mode TINYINT(1),
-                       kessel_soll SMALLINT(3),
-		       kessel_ist SMALLINT(3),       # 0,1
-		       kessel_abgas SMALLINT(3),     # 0,1
-		       warmwasser_soll TINYINT(2),
-		       warmwasser_offset TINYINT(2),
-		       warmwasser_ist SMALLINT(3),   # 0,1
-		       aussentemp SMALLINT(3),       # 0,1
-		       aussentemp_ged SMALLINT(3),   # 0,1
-		       starts INT,
-		       runtime INT,
-		       leistung TINYINT(3),           # %
-		       ventil TINYINT(1),
-		       pumpe TINYINT(3),             # %
-		       flow SMALLINT,                # l/h
-		       vorlauf_soll TINYINT(2),      # 1°
-		       );
+# Hier wird der Fehlerspeicher geloggt, aber auch nur wenn sich etwas
+# geändert hat:
+CREATE TABLE vitolog_errors (
+      timestamp TIMESTAMP CURRENT_TIMESTAMP NOT NULL,   # 4 byte
+      error1 TINYINT UNSIGNED NOT NULL,
+      error2 TINYINT UNSIGNED NOT NULL,
+      error3 TINYINT UNSIGNED NOT NULL,
+      error4 TINYINT UNSIGNED NOT NULL,
+      error5 TINYINT UNSIGNED NOT NULL,
+      error6 TINYINT UNSIGNED NOT NULL,
+      error7 TINYINT UNSIGNED NOT NULL,
+      error8 TINYINT UNSIGNED NOT NULL,
+      error9 TINYINT UNSIGNED NOT NULL,
+      error10 TINYINT UNSIGNED NOT NULL,
+      errortext VARCHAR(600) NOT NULL
+   );
 
