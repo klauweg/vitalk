e6d41efebda831095637728b3bf9273c6c9eae68
Klaus W <vortex25@gmx.de>
2013-09-17 10:46:50 +0200

    development - funktioniert noch nicht

--- vito_parameter.c
+++ vito_parameter.c
@@ -12,32 +12,28 @@
 #include "vito_io.h"
 #include "fehlerliste.h"
 
-// Hier wird die Cachezeit für die Parameterabfragen festgelegt.
-// Aufgrund der geringen Auflösung der time() Funktion kann es passieren, dass
-// bei CACHE_TIME == 0 trotzdem fast 1 sec. gewartet wird, bis neue Daten
-// gelesen werden. Es kann also bis zu 1000msec. länger dauern bis neue Daten
-// verfügbar sind als hier eingestellt ist:
-#define PARAM_CACHE_TIME 6
-
 // Das prologue() Makro wird verwendet, um den Anfang der
-// Parameterfunktionen zu bauen:
-#define prologue( address, length ) \
-   static char valuestr[40]; \
+// (meisten) Parameterfunktionen zu bauen:
+#define prologue( address, length, cache_time, bufferlen ) \
    static time_t old_time = 0; \
+   static char value_str[(bufferlen)]=""; \
+   time_t new_time; \
    uint8_t content[30]; \
-   if ( time( NULL ) > old_time + PARAM_CACHE_TIME ) \
+   new_time = time(NULL) / (cache_time); \
+   if ( new_time > old_time ) \
    { \
-     old_time = time( NULL ); \
+     old_time = new_time; \
      if ( vito_read( (address), (length), content ) < 0 ) \
-     sprintf( valuestr, "NULL" ); \
-   else \
-   { \
+       return -1; \
+     else \
+     { \
 
 // Das epilogue() Makro wird verwendet, um das Ende der
 // Parameterfunktionen zu bauen:
 #define epilogue() \
    } } \
-   return valuestr;
+   value_ptr = value_str; \
+   return 0;
 
 // Funktion zum interpretieren von 16bit Daten im content-array als
 // (signed) int16_t mit LSB first, MSB last (Viessmann Data Byteorder)
@@ -56,33 +52,30 @@ static void interpret_int16_t( uint8_t *content, char *valuestr )
 
 ////////////////////////// PARAMETERFUNKTIONEN ////////////////////
 
-// Hier folgt pro Parameter eine C-Funktion:
-// Rückgabewert bei den Lesefunktionen ist ein Pointer
-// auf ein char-array mit dem Ergebnistext.
-/////////////////// ALLGEMEIN
-char * read_deviceid( void )
+int read_deviceid( char *value_ptr )
 {
-  prologue( 0x00f8, 2 )
+  prologue( 0x00f8, 2, 240, 10 )
       // Normalerweise sind die Parameter in Little Endian
       // Byteorder, aber bei der Deviceid hat sich offenbar
       // die umgekehrte Interpretation durchgesetzt:
-      sprintf( valuestr, "0x%4x", (content[0] << 8) + content[1] );
+    sprintf( value_str, "0x%4x", (content[0] << 8) + content[1] );
   epilogue()
 }
 
-char * read_mode_numeric( void )
+int read_mode( char *value_ptr )
 {
-  prologue( 0x2323, 1 )
-    {
-      sprintf( valuestr, "%u", content[0] );
-    }
+  prologue( 0x2323, 1, 6, 5 )
+    sprintf( value_str, "%u", content[0] );
   epilogue()
 }
 
-int write_mode_numeric( int mode )
+int write_mode( char *value_str )
 {
-  uint8_t content[30];
-  // Dauernd reduziert und dauernd normal unterstützt meine Vitodens offenbar nicht
+  uint8_t content[10];
+  int mode;
+  
+  mode = atoi( value_str );
+  // Dauernd reduziert und dauernd normal unterstützt meine Vitodens offenbar nicht:
   if ( mode < 0 || mode > 2 )
     {
       fprintf( stderr, "Illegal Mode!\n");
@@ -93,82 +86,87 @@ int write_mode_numeric( int mode )
   return vito_write(0x2323, 1, content);
 }
 
-char * read_mode( void )
+int read_mode_text( char *value_ptr )
 {
-  prologue( 0x2323, 1 )
-    {
-      switch (content[0])
+  static char value_str[50] = "";
+  char mode[10];
+  
+  if ( read_mode( mode ) < 0 )
+    return -1;
+  
+  switch ( atoi(mode) )
 	{
-	case 0: strcpy( valuestr, "Abschaltbetrieb" );
+	case 0: strcpy( value_str, "Abschaltbetrieb" );
 	  break;
-	case 1: strcpy( valuestr, "Nur Warmwasser" );
+	case 1: strcpy( value_str, "Nur Warmwasser" );
 	  break;
-	case 2: strcpy( valuestr, "Heizen und Warmwasser" );
+	case 2: strcpy( value_str, "Heizen und Warmwasser" );
 	  break;
-	default: sprintf( valuestr, "UNKNOWN: %u", content[0] );
+	default: sprintf( value_str, "UNKNOWN: %u", content[0] );
 	  break;
 	}
-    }
-  epilogue()
+  
+  value_ptr = value_str;
+  return 0;
 }
 
-// Das auslesen der Fehlerliste ist etwas konfus, denn in dem
+// Das Auslesen der Fehlerliste ist etwas konfus, denn in dem
 // vito.xml file von ceteris paribus werden 9 byte pro Eintrag
 // gelesen. Ich sehe den Sinn aber nicht? Ich muss mal noch beobachten was passiert
 // wenn mehr als ein Eintrag in der Fehlerliste steht!
-char * read_error_history( void )
+int read_error_history( char *value_ptr )
 {
-  static char valuestr[10*80];
   static time_t old_time = 0;
-  uint8_t content;
-  int address;
+  static char value_str[80] = "";
+  time_t new_time;
+  uint8_t content[15];
+  int address, i;
+  
+  new_time = time(NULL) / 60;
+  if ( new_time > old_time )
+    {
+      old_time = new_time;
+      
+      // Die 10 Fehlermeldungen numerisch ins content Array lesen:
+      i = 0;
+      for ( address = 0x7507; address <= 0x7558; address += 9 )
+	if ( vito_read( address, 1, &content[i++] ) < 0 )
+	  return -1;
+   
+      // String der Form 0,0,0,0,0,0,0,0,0,0 basteln:
+      value_str[0] = '\0';
+      for ( i = 0, i <= 9 ; i++ )
+	sprintf( value_str,"%s%u%c", value_str, content[i], i == 9 ? '\n' : ',' );
+    }
+  
+  value_ptr = value_str;
+  return 0;
+}
 
-  if ( time( NULL ) > old_time + PARAM_CACHE_TIME )
-    {
-      old_time = time( NULL );
-      strcpy( valuestr, "" );
-      for ( address = 0x7507; address <= 0x7558; address += 9 )
-	{
-	  if ( vito_read( address, 1, &content ) < 0 )
-	    strcat( valuestr, "NULL\n" );
-	  else
-	    {
-	      sprintf( valuestr, "%s0x%02x %s                   \n", 
-		       valuestr, content, fehlerliste[content] );
-	    }
-	}
-    }
-  
-  return valuestr;
-}
-// Das ganze nochmal in numerischer Form:
-char * read_error_history_numeric( void )
+int read_error_history_text( char *value_ptr )
 {
-  static char valuestr[80];
-  static time_t old_time = 0;
-  uint8_t content[10];
-  int address;
-  int i=0;
+  static char value_str[80*10] = "";
+  char errors[80] = "";
+  char *error[15];
+  int i;
   
-  if ( time( NULL ) > old_time + PARAM_CACHE_TIME )
-    {
-      old_time = time( NULL );
-      strcpy( valuestr, "" );
-      for ( address = 0x7507; address <= 0x7558; address += 9 )
-	{
-	  if ( vito_read( address, 1, &content[i++] ) < 0 )
-	    {
-	      strcpy( valuestr, "NULL\n" );
-	      return valuestr;
-	    }
-	}
-      sprintf( valuestr,"%d,%d,%d,%d,%d,%d,%d,%d,%d,%d\n",
-	       content[0],content[1],content[2],content[3],content[4],
-	       content[5],content[6],content[7],content[8],content[9] );
-    }
+  if ( read_error_history( errors ) < 0 )
+    return -1;
   
-  return valuestr;
+  // parse string from read_error_history():
+  error[0] = strtok( errors, "," )
+  for ( i=1; i<=9; i++ )
+    error[i] = strtok( NULL, "," );
+  
+  // String mit 10 Zeilen der Fehlerbeschreibungen basteln:
+  value_str[0]='\0';
+  for ( i=0; i<=9; i++ )
+    sprintf( value_str, "%s0x%02x %s                   \n", 
+		       value_str, atoi(error[i]), fehlerliste[atoi(error[i])] );
+  value_ptr = value_str;
+  return 0;
 }
+
 
 //////////////////// KESSEL
 char * read_K_abgas_temp( void )
@@ -421,3 +419,51 @@ char * read_niveau( void )
     sprintf( valuestr, "%u", content[0] );
   epilogue()
 }
+
+
+//////////////////////////////////////////////////////////////////////////
+
+// Struktur zur Verwaltung der Parameter
+struct s_parameter {
+  char *p_name;         // Parameter Kurzname
+  char *p_description;  // Beschreibung des Parameters
+  char *p_einheit;      // Einheit (String)
+  int *p_class;         // Parameterklasse, siehe #define oben
+  int (*f_read) (char *valuestr);   // Funktion zum lesen aus der Vitodens
+  int (*f_write) (char *valuestr);  // Funktion zum Schreiben in die Vitodens
+};
+
+const static struct s_parameter parameter_liste[] = {
+  { "deviceid", "Geraeteidentifikation", "", P_GENERAL, &read_deviceid, NULL },
+  { "mode", "Betriebsmodus (numerisch)", "", P_GENERAL, &read_mode, &write_mode },
+  { "mode_text", "Betriebsmodus (text)", "", P_GENERAL, &read_mode_text, NULL },
+  { "errors", "Error History (numerisch)", "", P_GENERAL, &read_errors, NULL },
+  { "errors_text", "Error History (text)", "", P_GENERAL, &read_errors_text, NULL },
+  { "outdoor_temp", "Aussentemperatur", "°C", P_GENERAL, &read_outdoor_temp, NULL },
+  { "outdoor_temp_tp", "Aussentemperatur Tiefpass", "°C", P_GENERAL, &read_outdoor_temp_tp, NULL },
+  { "outdoor_temp_smooth", "Aussentemperatur Gedämpft", "°C", P_GENERAL, &read_outdoor_temp_smooth, NULL },
+  { "k_ist_temp", "Kessel Ist Temperatur", "°C", P_KESSEL, &read_k_ist_temp, NULL },
+  { "k_ist_temp_tp", "Kessel Ist T. nach Tiefpass", "°C", P_KESSEL, &read_k_ist_temp_tp, NULL },
+  { "k_soll_temp", "Kessel Soll Temperatur", "°C", P_KESSEL, &read_k_soll_temp, NULL },
+  { "k_abgas_temp", "Kessel Abgastemperatur", "°C", P_KESSEL, &read_abgas_temp, NULL },
+  { "ww_soll_temp", "Warmwasser Soll Temperatur", "°C", P_WARMWASSER, &read_ww_soll_temp, &write_ww_soll_temp },
+  { "ww_ist_temp", "Warmwasser Ist Temperatur", "°C", P_WARMWASSER, &read_ww_ist_temp, NULL },
+  { "ww_ist_temp_tp", "Warmwasser Ist Temp. Tiefpass", "°C", P_WARMWASSER, &read_ww_ist_temp_tp, NULL },
+  { "ww_offset", "Offset Kessel/WW Soll", "K", P_WARMWASSER, &read_ww_offset, NULL },
+  { "starts", "Brennerstarts", "", P_BRENNER, &read_starts, NULL },
+  { "starts_h", "Brennerlaufzeit", "h", P_BRENNER, &read_runtime_h, NULL },
+  { "runtime", "Brennerlaufzeit", "s", P_BRENNER, &read_runtime, NULL },
+  { "power", "Brennerleistung", "%%", P_BRENNER, &read_power, NULL },
+  { "ventil", "Ventilstellung", "", P_HYDRAULIK, &read_ventil, NULL },
+  { "ventil_text", "Ventilstellung", "", P_HYDRAULIK, &read_ventil_text, NULL },
+  { "pump_power", "Pumpenleistung", "%%", P_HYDRAULIK, &read_pump_power, NULL },
+  { "flow", "Volumenstrom", "l/h", P_HYDRAULIK, &read_flow, NULL },
+  { "vl_soll_temp", "Vorlauf Solltemperatur", "°C", P_HEIZKREIS, &read_vl_soll_temp, NULL },
+  { "raum_soll_temp", "Raum Solltemperatur", "°C", P_HEIZKREIS, &read_raum_soll_temp, &write_raum_soll_temp },
+  { "red_raum_soll_temp", "Reduzierte Raum Solltemperatur", "°C", P_HEIZKREIS, &read_red_raum_soll_temp, &write_red_raum_soll_temp },
+  { "niveau", "Heizkurve Niveau", "K", P_HEIZKREIS, &read_niveau, NULL },
+  { "neigung", "Heizkurve Neigung", "", P_HEIZKREIS, &read_neigung, NULL },
+  { "", "", 0, NULL, NULL }
+};
+
+  
--- vito_parameter.h
+++ vito_parameter.h
@@ -1,46 +1,9 @@
-// ALLGEMEIN
-char * read_deviceid( void );
-char * read_mode_numeric( void );
-int write_mode_numeric( int mode );
-char * read_mode( void );
-char * read_error_history( void );
-char * read_error_history_numeric( void );
 
-// KESSEL
-char * read_K_abgas_temp( void ); // °C
-char * read_K_ist_temp( void ); // °C
-char * read_K_istTP_temp( void ); // °C
-char * read_K_soll_temp( void ); // °C
+// Parameterklassen:
+#define P_ALLGEMEIN  1
+#define P_KESSEL     2
+#define P_WARMWASSER 3
+#define P_HEIZKREIS  4
+#define P_BRENNER    5
+#define P_HYDRAULIK  6
 
-// WARMWASSER
-char * read_WW_soll_temp( void); // °C
-int write_WW_soll_temp( int temp );
-char * read_WW_offset( void ); // K
-char * read_WW_istTP_temp( void ); // °C
-char * read_WW_ist_temp( void ); // °C
-
-// AUSSENTEMPERATUR
-char * read_outdoor_TP_temp( void ); // °C
-char * read_outdoor_smooth_temp( void ); // °C
-char * read_outdoor_temp( void ); // °C
-
-// BRENNER
-char * read_starts( void ); // Zähler
-char * read_runtime( void ); // Laufzeit in Sekunden
-char * read_runtime_h( void ); // Laufzeit in Stunden
-char * read_power( void ); // Leistung in %
-
-// HYDRAULIK
-char * read_ventil_numeric( void );
-char * read_ventil( void );
-char * read_pump_power( void ); // %
-char * read_flow( void ); // l/h
-
-// HEIZKREIS
-char * read_VL_soll_temp( void ); // °C
-char * read_raum_soll_temp( void); // °C
-int write_raum_soll_temp( int temp ); // °C
-char * read_red_raum_soll_temp( void); // °C
-int write_red_raum_soll_temp( int temp ); // °C
-char * read_neigung( void );
-char * read_niveau( void );
